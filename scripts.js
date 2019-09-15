1b099ac698850a34a637ae981ffe875a2942a129
eempc <evc484@gmail.com>
2019-09-15 15:10:11 +0100

    disappearing cookie button

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-var startDate = document.getElementById('start-date');
+const startDate = document.getElementById('start-date');
 startDate.valueAsDate = new Date();
 
-var endDate = document.getElementById('end-date');
+const endDate = document.getElementById('end-date');
 endDate.valueAsDate = new Date();
+
+function cookieDisappear() {
+    const cookieArea = document.getElementById('cookie');
+    cookieArea.style.display = "none";
+}
