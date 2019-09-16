aed6d5dd4119bc6546600c10053f8fd629a8dc0e
Evelyn <evc484@gmail.com>
2019-09-16 14:17:33 +0100

    local storage test

--- scripts.js
+++ scripts.js
@@ -1,22 +1,30 @@
-let dateToday = new Date();
+let today = new Date();
 
 // Setting the form dates
 const startDate = document.getElementById('start-date');
-startDate.valueAsDate = dateToday;
+startDate.valueAsDate = today;
 
 const endDate = document.getElementById('end-date');
-endDate.valueAsDate = dateToday;
+endDate.valueAsDate = today;
 
-// Leave a cookie
+
+
+// Cookies and settings
 
 function cookieDisappear() {
     //document.cookie = "kelvincookie=; expires=Tue, 19 Sep 2019 12:00:00 UTC; path=/;";
     //document.cookie = "kelvincookie2=5; expires=Tue, 19 Sep 2019 12:00:00 UTC; path=/;";
     //alert(document.cookie);
-    alert(getCookieValue("kelvincookie"));
+    //alert(getCookieValue("kelvincookie2"));
+    localStorage.acceptedCookie = true;
+    let expiry = new Date();
+    expiry.setTime(today.getTime() + 3600000 * 24 * 14);
+    localStorage.expiryDate = expiry;
+    alert(localStorage.expiryDate);
+
     
     const cookieArea = document.getElementById('cookie');
-    cookieArea.style.bottom = "-50px";
+    cookieArea.style.bottom = "-100px";
 }
 
 // This assumes that cookies are set as follows: key=value, with no space before or after the = sign
@@ -24,7 +32,7 @@ function getCookieValue(cookieName) {
     // Get the cookie in its entirety then use the inbuilt method to decode the cookie then split the cookie string by ;
     let keyValuesSplit = decodeURIComponent(document.cookie).split(';');
 
-    // Simple search method, but a regex method may also be possible in lieu of this methods
+    // Simple linear search method, but a regex method may also be possible in lieu of this methods
     for (let i = 0; i < keyValuesSplit.length; i++) {
         let keyValuePair = keyValuesSplit[i].trim();
         if (keyValuePair.indexOf(cookieName) == 0) {
@@ -34,3 +42,5 @@ function getCookieValue(cookieName) {
 
     return "";
 }
+
+// Local Web storage methods supercede?
